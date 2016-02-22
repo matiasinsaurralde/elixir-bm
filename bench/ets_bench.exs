6c1e4e5d13b2950339c19191564dd8c99d64d1f1
Matias Insaurralde <matias@insaurral.de>
2016-02-22 10:18:55 -0300

    Insertion bench

--- bench/ets_bench.exs
+++ bench/ets_bench.exs
@@ -3,7 +3,27 @@ defmodule ETSBench do
 
   @table :sample_table
 
-  bench "hello list" do
+  before_each_bench _ do
+    :ets.new( @table, [ :set, :public, :named_table ] )
+    { :ok, nil }
+  end
+
+  bench "insert one item" do
+    key = "sampleKey"
+    item = %{}
+
+    :ets.insert( @table, { key, item } )
+
+  end
+
+  bench "insert five items" do
+    Enum.each [1, 2, 3, 4, 5], fn n ->
+      key = "sampleKey#{n}"
+      item = 1
+
+      :ets.insert( @table, { key, item } )
+
+    end
   end
 
 end
